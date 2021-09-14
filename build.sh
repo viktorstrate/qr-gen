7a0e266243989ecfb9cdb2274b0625270c6563cb
viktorstrate <viktorstrate@gmail.com>
2021-09-14 13:06:07 +0200

    Optimize builds

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+js="dist/elm.js"
+min="dist/elm.min.js"
+
+mkdir -p dist
+cp index.html dist/
+
+elm make --optimize --output=$js src/Main.elm
+
+npx uglify-js $js --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' | npx uglify-js --mangle --output $min
+
+echo "Compiled size:$(wc -c $js) bytes  ($js)"
+echo "Minified size:$(wc -c $min) bytes  ($min)"
+echo "Gzipped size: $(gzip -c $min | wc -c) bytes"
